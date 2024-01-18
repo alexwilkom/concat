8bbc7ac46ede74ebef9dedb455f7c11e7648cb65
Alex Wilkom <alex.wilkom@icloud.com>
2024-01-18 20:55:48 +0100

    Create helper functions to read and print files

--- concat.py
+++ concat.py
@@ -1,3 +1,19 @@
 import sys
 import os
 
+def read(source_file):
+    try:
+        with open(source_file, 'r') as source:
+            return source.read()
+    except FileNotFoundError:
+        print("File not found. Try checking the file path.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def print_file(source):
+    try:
+        print(read(source))
+    except FileNotFoundError:
+        print("File not found. Try checking the file path.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
