aa0d71b48510d1fc16bd50e5eea041cff26726fe
Alex Wilkom <alex.wilkom@icloud.com>
2024-01-19 09:54:10 +0100

    Add numbered_lines function

--- concat.py
+++ concat.py
@@ -29,3 +29,15 @@ def append_file(source_file, destination_file):
         print("One or both of the files not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+def numbered_lines(input, option):
+    if os.path.isfile(input):
+        input = read(input)
+    text_array = input.splitlines()
+    if option == "-n":
+        for i, line in enumerate(text_array, start=1):
+            print(f"{i} {line}")
+    if option == "-b":
+        for i, line in enumerate(filter(None, text_array), start=1):
+            print(f"{i} {line}")
+            print()
