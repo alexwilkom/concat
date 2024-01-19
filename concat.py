9d7802ae6a711f29d314ad964efff2cb3c646220
Alex Wilkom <alex.wilkom@icloud.com>
2024-01-19 09:30:04 +0100

    add append_file function

--- concat.py
+++ concat.py
@@ -17,3 +17,14 @@ def print_file(source):
         print("File not found. Try checking the file path.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+def append_file(source_file, destination_file):
+    try:
+        content = read(source_file)
+        with open(destination_file, 'a') as destination:
+            destination.write(content)
+        print(f"Content from '{source_file}' appended to '{destination_file}' successfully.\n")
+    except FileNotFoundError:
+        print("One or both of the files not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
